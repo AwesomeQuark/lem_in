73b7afbf80b8f4b2258fa1dab6a0a675967d1fb9
corentin <corentin.noel@aliceadsl.fr>
2019-03-20 17:12:51 +0100

    added get_next_open_link

--- include/lem_in.h
+++ include/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: conoel <conoel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/16 18:53:12 by conoel            #+#    #+#             */
-/*   Updated: 2019/03/20 16:14:28 by conoel           ###   ########.fr       */
+/*   Updated: 2019/03/20 17:12:45 by conoel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ int			verify_data(char *data);
 t_node			*get_start(t_node *head);
 t_node			*get_end(t_node *head);
 t_node			*get_node(char *name, size_t len, t_node *head);
+t_node			*get_next_open_link(t_node *node);
 
 #endif
