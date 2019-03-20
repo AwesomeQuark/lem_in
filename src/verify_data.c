155bb2a0270c59005f3826f429bc1f33bc3275a6
corentin <corentin.noel@aliceadsl.fr>
2019-03-20 13:08:42 +0100

    added simple input filtering, detect almost all basic errors

--- src/verify_data.c
+++ src/verify_data.c
@@ -6,26 +6,49 @@
 /*   By: conoel <conoel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/19 14:50:26 by conoel            #+#    #+#             */
-/*   Updated: 2019/03/19 18:08:56 by conoel           ###   ########.fr       */
+/*   Updated: 2019/03/20 13:07:59 by conoel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/lem_in.h"
 
-int		verify_data(char *data)
+static int	error(int line, int type, char c)
+{
+	ft_printf("Error line %d [char: %c] [type: %d]\n", line, c, type);
+	return (0);
+}
+
+int			verify_data(char *data)
 {
 	size_t	i;
-	size_t	j;
+	int		line;
 
 	i = 0;
+	line = 2;
+	ft_strtoll(&data[i], &i, 10);
+	if (data[i++] != '\n')
+		return (error(line, 1, data[i]));
 	while (data[i])
 	{
-		j = 0;
-		while (data[i + j] != '\n' && data[i + j])
-			j++;
-		if (ft_memchr(&data[i], ' ', j) == NULL && data[i] != '#')
-			break ;
-		i = i + j + 1;
+		if (data[i] == '#')
+			while (data[i] != '\n')
+				i++;
+		else
+		{
+			while (data[i] != ' ')
+			{
+				if (data[i] == '-')
+					return (1);
+				i++;
+			}
+			ft_strtoll(&data[i], &i, 10);
+			if (data[i++] != ' ')
+				return (error(line, 2, data[i]));
+			ft_strtoll(&data[i], &i, 10);
+		}
+		if (data[i++] != '\n')
+			return (error(line, 3, data[i]));
+		line++;
 	}
 	return (1);
 }
